0a8ec9b8a9cabab7712f67d251dce15597d506e3
dorje75 <dhondupnerchung@gmail.com>
2024-09-18 19:53:38 +0530

    updated css

--- mainBot/Chatbot-Widget/static/js/script.js
+++ mainBot/Chatbot-Widget/static/js/script.js
@@ -8,8 +8,50 @@ function include(file) {
 
   document.getElementsByTagName('head').item(0).appendChild(script);
 }
+/* import components */
+include('./static/js/components/index.js');
 
 
+//#########Temporary 1##############################>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Function to show the form and overlay, and disable chat input
+function showForm() {
+  document.getElementById('formTable').style.display = 'block';
+  document.getElementById('overlay').style.display = 'block';
+  document.querySelector('.chats').style.pointerEvents = 'none'; // Disable interaction with chat
+  document.getElementById('userInput').disabled = true; // Disable chat input
+}
+
+// Function to hide the form and overlay, and enable chat input
+function hideForm() {
+  document.getElementById('formTable').style.display = 'none';
+  document.getElementById('overlay').style.display = 'none';
+  document.querySelector('.chats').style.pointerEvents = 'auto'; // Enable interaction with chat
+  document.getElementById('userInput').disabled = false; // Enable chat input
+}
+
+// Example function to handle form submission
+function submitForm() {
+  const formData = {
+      male_count: document.getElementById('male_count').value,
+      female_count: document.getElementById('female_count').value,
+      children_count: document.getElementById('children_count').value,
+  };
+
+  // Send form data to Rasa bot
+  sendMessageToRasa('form_submission', formData);
+
+  // Hide the form after submission
+  hideForm();
+}
+
+// Function to handle bot actions
+function handleBotAction(action) {
+  if (action === 'trigger_date_form') {
+      showForm();
+  }
+}
+
+//#######################Temporary 1<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 // Bot pop-up intro
 document.addEventListener("DOMContentLoaded", () => {
   const elemsTap = document.querySelector(".tap-target");
@@ -21,8 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }, 4000);
 });
 
-/* import components */
-include('./static/js/components/index.js');
+
 
 window.addEventListener('load', () => {
   // initialization
